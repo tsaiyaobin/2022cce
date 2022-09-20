5452e24c1dfbf1d57ccf2fc0605904175bd61f5b
蔡耀斌 <112918606+tsaiyaobin@users.noreply.github.com>
2022-09-20 19:21:31 +0800

    week03

---
+++ week03-3.cpp
@@ -0,0 +1,13 @@
+///week03-4.cpp
+///要比大小(第二章、第三章)
+#include <stdio.h>
+int main()
+{
+    printf("請輸入數字n:");
+
+    int n;
+    scanf("%d",&n);
+    if(n>10)printf("n>10");
+    if(n<10)printf("n<10");
+    if(n==10)printf("n==10");
+}
