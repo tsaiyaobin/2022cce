9fc82388b63b9bfd604a94b59ba08193fc0cf80a
蔡耀斌 <112918606+tsaiyaobin@users.noreply.github.com>
2022-10-18 12:16:32 +0800

    week07

---
+++ week07-4.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf(":%d\n",n%10);
+    n = n / 10;
+
+    printf("現在的個位數:%d\n",n%10);
+    n = n / 10;
+
+    printf("現在的個位數:%d\n",n%10);
+    n = n / 10;
+
+    printf("現在的個位數:%d\n",n%10);
+    n = n / 10;
+
+    printf("現在的個位數:%d\n",n%10);
+    n = n / 10;
+
+    printf("現在的個位數:%d\n",n%10);
+    n = n / 10;
+
+    printf("現在的個位數:%d\n",n%10);
+    n = n / 10;
+
+    printf("現在的個位數:%d\n",n%10);
+    n = n / 10;
+
+    printf("現在的個位數:%d\n",n%10);
+    n = n / 10;
+
+    printf("現在的個位數:%d\n",n%10);
+    n = n / 10;
+
+}
+
